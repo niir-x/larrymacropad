f9be8abe891e40b7bcfb51ad4af992cf5a40b7a3
niir-x <annoyingakortox@gmail.com>
2026-03-29 19:21:17 -0500

    Add files via upload

---
+++ Firmware/main.py
@@ -0,0 +1,25 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+
+keyboard = KMKKeyboard()
+
+macros = Macros()
+keyboard.modules.append(macros)
+
+PINS = [board.D3, board.D4, board.D2, board.D1]
+
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+keyboard.keymap = [
+    [KC.DELETE, KC.RIGHT, KC.LEFT, KC.PRNTSC,]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
